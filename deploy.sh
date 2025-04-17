2bdc94f46b5e2af6335c3db169240289fb1836af
Prasanna Ramesh <114574731+Prasannaramesh13@users.noreply.github.com>
2025-04-17 21:20:08 +0530

    initial commit

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 echo "Deploying to server..."
 
 # Accept BRANCH_NAME as an argument
-BRANCH_NAME=$(basename "$1")
-echo "Raw input branch: $1"
-echo "Normalized branch: $BRANCH_NAME"
+BRANCH_NAME="$1"
+echo "Received branch name: $BRANCH_NAME"
+
 # Set image and container name based on branch
 if [ "$BRANCH_NAME" == "main" ]; then
     IMAGE="prasanna1808/prod:latest"
@@ -17,8 +17,10 @@ fi
 
 # Connect to EC2 and deploy
 ssh -o StrictHostKeyChecking=no ubuntu@52.71.59.200 << EOF
-    docker pull $IMAGE
-    docker stop $CONTAINER_NAME || true
-    docker rm $CONTAINER_NAME || true
-    docker run -d --name $CONTAINER_NAME -p 80:3000 $IMAGE
+  echo "Pulling image: $IMAGE"
+  docker pull $IMAGE
+  docker stop $CONTAINER_NAME || true
+  docker rm $CONTAINER_NAME || true
+  docker run -d --name $CONTAINER_NAME -p 80:3000 $IMAGE
 EOF
+
