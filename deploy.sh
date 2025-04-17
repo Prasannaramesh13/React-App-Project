24da673abc90fd55957abf6d0b644cd3b1c5e427
Prasanna Ramesh <114574731+Prasannaramesh13@users.noreply.github.com>
2025-04-17 21:27:44 +0530

    initial commit

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,11 @@
 echo "Deploying to server..."
 
 # Accept BRANCH_NAME as an argument
-BRANCH_NAME=$1
+BRANCH_NAME="$1"
+echo "Received branch name: $BRANCH_NAME"
 
 # Set image and container name based on branch
-if [ "$BRANCH_NAME" == "main" ] || [ "$BRANCH_NAME" == "prod" ]; then
+if [ "$BRANCH_NAME" == "main" ]; then
     IMAGE="prasanna1808/prod:latest"
     CONTAINER_NAME="app-prod"
 else
@@ -16,8 +17,9 @@ fi
 
 # Connect to EC2 and deploy
 ssh -o StrictHostKeyChecking=no ubuntu@52.71.59.200 << EOF
-    docker pull $IMAGE
-    docker stop $CONTAINER_NAME || true
-    docker rm $CONTAINER_NAME || true
-    docker run -d --name $CONTAINER_NAME -p 80:3000 $IMAGE
+  echo "Pulling image: $IMAGE"
+  docker pull $IMAGE
+  docker stop $CONTAINER_NAME || true
+  docker rm $CONTAINER_NAME || true
+  docker run -d --name $CONTAINER_NAME -p 80:3000 $IMAGE
 EOF
